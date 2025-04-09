f7cbaa277643b34724abad78fbd637ef444565ad
Jatin_Mehta <mehtajatin045@gmail.com>
2025-04-09 22:47:15 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,6 @@ function App() {
               "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
             )}
           />
-          {/* Radial gradient for the container to give a faded look */}
           <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
           
     <div className="relative p-6 max-w-4xl mx-auto">
